b300f87181cfaf608d8a3fd0db79604b8b687ddc
Jason Mills <jason.mills@integratelecom.com>
2016-09-11 12:26:09 -0700

    define some stub default dummy drivers

--- usumu/bus/__init__.py
+++ usumu/bus/__init__.py
@@ -1,9 +1,13 @@
-import abc
+from abc import ABCMeta, abstractmethod
 
 
-class Bus(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
+class Bus(metaclass=ABCMeta):
+    @abstractmethod
     def publish(self, msg):
         """Publishes message to bus"""
         pass
 
+
+class Default(Bus):  # default bus driver is a dummy driver, it does not do anything
+    def publish(self, msg):
+        pass
--- usumu/logger/__init__.py
+++ usumu/logger/__init__.py
@@ -4,3 +4,6 @@ from abc import ABCMeta
 class Logger(metaclass=ABCMeta):
     pass
 
+
+class Default(Logger):  # default logger driver is a dummy driver and does nothing
+    pass
